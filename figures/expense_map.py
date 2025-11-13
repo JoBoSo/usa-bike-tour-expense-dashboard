62e5cca425927adf899f5fc907504fe7efa75ff4
JoBoSo <jamesbscott@hotmail.ca>
2025-11-12 21:57:44 -0500

    colored markers in expense map based on category

--- figures/expense_map.py
+++ figures/expense_map.py
@@ -4,13 +4,26 @@ import numpy as np
 
 df = dfs.expenses.to_pandas()
 
+color_map = {
+    'bike maintenance': '#CCFF00',
+    'border entrance fee': '#FFA500',
+    'camping': '#FF00FF',
+    'food and drink': '#00FFFF',
+    'lodging': '#FF0000',
+    'movie theatre': '#FFFF00',
+    'shower': '#800080',
+    'thrift shop': '#FFFFFF',
+    'transportation': '#000000',
+    'travel insurance': '#008080',
+}
+
 fig = go.Figure(go.Scattermap(
     lat=df['latitude'],
     lon=df['longitude'],
     mode='markers', # Or 'lines', 'lines+markers'
     marker=dict(
         size=np.maximum(np.log(df['cost_cad'])*2.5, 4), 
-        color='#00FFFF',
+        color=df['category'].map(color_map),
     ),
     text=df[['store_name', 'cost_cad', 'category']]
         .apply(lambda row: f"{row['store_name']}<br>Cost: ${row['cost_cad']:.2f}<br>Category: {row['category']}", axis=1),
